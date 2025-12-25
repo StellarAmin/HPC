8022aed17238c2ad1d94672ff472a14c779f3956
Amin Mossadag <amin.moss@gmail.com>
2025-12-25 23:48:15 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/amin/Final Assignment
-# Build directory: D:/amin/Final Assignment/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
